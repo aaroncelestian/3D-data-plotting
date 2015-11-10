accfbfe256dd9b5f42cdd66fdd33e4e8581743eb
Aaron Celestian <aaron.celestian@gmail.com>
2015-11-10 12:08:34 -0600

    added data checks and dir selection
    Added:
    
     - check to see if '#NaN' strings are in the data frame.  I ran into an instance where there were and it would not plot.  Note, the thermo DXR Raman microscope puts #NaN instead of a normal NaN string value.  Kind of annoying since python has nice functions for finding NaN, but no nice functions for finding #NaN.  So, I had to make a workaround and added a definition called   stringCheck.  stringCheck will print info to the screen letting you know if your dataFrame contains strings or not.
    
    - Users can now graphically select the file and directory they want to use!  this is super helpful, and now provides a better experience with less errors or confusion.

--- Import_CSV_files_into_pandas.py
+++ Import_CSV_files_into_pandas.py
@@ -1,19 +1,38 @@
 import glob
-#import os  # presumable I can use this to enable me to run this from any dir
+import os  
 import pandas as pd
 import numpy as np
-#from pylab import *
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
-
-## Version 9.23.2015
+import wx
 
 
+### version 11/10/2015
 
-filelist = pd.read_csv('datafile.CSV')  #put directory/file name here: imports a CSV file into pandas, I need a non-empty pandas to append things to
-##### how to I read just any file?  It shouldn't matter which file since they all have the same X values
-##### it would be nice to just run this code and then select a file....
-##### and also, how can I request the user to select the working directory???
+def get_path(wildcard):
+    app = wx.App(None)
+    style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
+    dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
+    if dialog.ShowModal() == wx.ID_OK:
+        path = dialog.GetPath()
+    else:
+        path = None
+    dialog.Destroy()
+    return path
+
+
+abspath = os.path.abspath(get_path('*.csv')) #prompts user to go find the file
+dname = os.path.dirname(abspath) # gets the directory of that file
+os.chdir(dname) # changes working directory to where the file was grabbed from
+
+print ""
+print os.getcwd()
+print abspath
+print ""
+
+filelist = pd.read_csv(abspath)
+
+
+
+
 
 
 df1_x_name = filelist.columns.values[0]  #finds the column header for the [0]
@@ -22,16 +41,17 @@ df1_y_name = filelist.columns.values[1]  #finds the colunm header for the [1]
 filelist=filelist.rename(columns={df1_x_name:'X'}) # renames [0] to 'X'
 filelist=filelist.rename(columns={df1_y_name:'Y'}) # renames [1] to 'Y'
 
-filelist = filelist.drop('Y', 1)  # now we delete the y column because we are going to read again below /
-                                  # I can probably just do this in the above above, but this shows what I'm trying to do.  
+filelist = filelist.drop('Y', 1)  # now we delete the y column because we are going to read again below
+                                  # I can probably just do this set above, but it shows what I'm trying to do  
 Xfilelist = filelist.drop('X', 1)
 
 
-###
+##
 ### ok, now I start building a large pandas with all the data in it.  
 ### Each new column will be the Y column of each imported csv data file
 ###
 
+numFiles = 0 # reset the counter for the number of files processed (numFiles)
 for counter, files in enumerate(glob.glob("*.CSV")):  #finds all CSV files in the current working folder
     
     newfile = pd.read_csv(files)          # Read a CSV file, presumable the first in alpha/numerical order
@@ -40,13 +60,14 @@ for counter, files in enumerate(glob.glob("*.CSV")):  #finds all CSV files in th
     numFiles = counter                      # total number files itterated, scaler
     df1_y_name = newfile.columns.values[1]  # finds column header name for [1]
     df1_x_name = newfile.columns.values[0]
+    #if 
     
     newfile=newfile.rename(columns={df1_y_name:counter})  # renames column header to current counter #
     #newfile=newfile.rename(columns={df1_x_name:counter})
     
-    filelist = filelist.join(newfile[counter], how='right', rsuffix='a') # joins 'counter #' column to the main pandas list /
-    Xfilelist = Xfilelist.join(filelist[counter], how='right', rsuffix='x')  # here I want to make a pandas for the X values /
-                                                   #with the same number of columns as the Y values
+    filelist = filelist.join(newfile[counter], how='right', rsuffix='a') # joins 'counter #' column to the main pandas list
+    #Xfilelist = Xfilelist.join(filelist[counter], how='right', rsuffix='x')  # here I want to make a pandas for the X values
+                                                                        #with the same number of columns as the Y values.  why?  I don't know yet
     
     
 #filelist.to_csv('./other_files/AllData.CSV')  # uncomment to save the filelist csv file
@@ -61,25 +82,61 @@ X = pd.DataFrame(X)   #convert it to a pandas
 Y = filelist[0::]            # Y is already a panda, y values are the intensity data for each data file
 Y = Y.drop('X', 1)           # now I delete the 'X' because those values are in the xx varible
 
-#Z = range(0,(numFiles),1)    #makes a list of from 0 to numFiles as a z scaler, need this to itterate over
-#Z = pd.Series(Z)             #convert it to a pandas
+
+def stringCheck(df):
+    # Check to see if there are missing values if the dataframe... 
+    #'#NaN' is used with the Thermo DXR raman microscope
+    HowManyOjects = 0
+    for col in df:
+        if df[col].dtypes == object:
+            HowManyOjects = HowManyOjects + 1
+            #print HowManyOjects
+            
+        elif df[col].dtypes != object:
+            HowManyOjects = HowManyOjects + 0
+
+            
+    # now we figure out if there are any strings, aka objects        
+    if HowManyOjects > 0:
+        return True
+    else:
+        return False
+            
+    
+        
+
+if stringCheck(Y) == True:
+    print ""
+    print "Warning, there are strings in your data."
+    print "This is likely because of some missing values during the original saving process on your instrument."
+    print "I will try to correct this, but you should check your Y.pkl file to be sure."
+    print ""
+    # If there are NaN or #NaN entries, then this will fix it
+    # If I decide to use this with more than one type of data, I should make a python list of things to replace
+    Y2 = Y.replace('#NaN',0)
+    Y2.to_pickle('Y.pkl') 
+else:
+    print ""
+    print "The data looks good.  No missing values are seen."
+    print ""
+    Y.to_pickle('Y.pkl')
+    
+
 
 X.to_pickle('X.pkl')
-Y.to_pickle('Y.pkl')
-#Z.to_pickle('Z.pkl')
+#Z.to_pickle('Z.pkl')   #This isn't needed here, I make a new instance of Z when plotting data
 
-# maxYlist simply grabs the highest intensity value for all of your patterns, and puts it in a list.  
 maxYlist = []
 for i in Y.columns:
     maxYlist.append(max(Y[i].values))
 
 print ""
 print "SUCCESS: X.pkl and Y.pkl created in the working directory."
-print "Number of files proccessed: ", max(Y.index.values)
+print "Number of files proccessed: ", numFiles+1 #max(Y.index.values)
 print "Data range X axis:          ", min(X.values), " to", max(X.values)
 print "Max intensity:              ", max(maxYlist)
 
 
 
 
-### plotting has been moved to another py file
+### plotting has been moved to another py file...  plotting_data.py
